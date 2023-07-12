52a7e1c14047c6ba0feed70eac7fc6c47b7dd226
André Gonçalves <andregoncalves20007@gmail.com>
2023-07-12 21:48:50 +0100

    Add wip

---
+++ _next/static/lXdpDORrC0d070VXpIYLk/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/be3bca439d8cf5c9.css","static/chunks/pages/index-5521cf6aea59f515.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
